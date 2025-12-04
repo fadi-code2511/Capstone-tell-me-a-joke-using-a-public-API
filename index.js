dd621fca9162dc890379420896c9ef5955afa8ca
Fadi nasr <nasr.fadi2511@gmail.com>
2025-12-04 17:52:51 +0200

    completed api any joke test

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.get("/",async(req,res)=>{
     try {
         const response=await axios.get(apiUrl);
         const result=response.data;
-        res.render("index.ejs",{joke:result.setup,answer:result.delivery})
+        console.log(result.type)
+        res.render("index.ejs",{joke:result});
     } catch (error) {
         // console.log(error)
         const statusCode = error.response?.status || 500;

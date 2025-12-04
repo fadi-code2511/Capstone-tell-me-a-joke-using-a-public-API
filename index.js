e17d7a8b21dc7096ea73387673bcf07764512e4d
Fadi nasr <nasr.fadi2511@gmail.com>
2025-12-04 20:21:03 +0200

    adding selector to get joke by category

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import axios from "axios"
 
 const app =express();
 const port= 3000;
-const apiUrl="https://v2.jokeapi.dev/joke/Any";
+const apiUrl="https://v2.jokeapi.dev/joke/";
 
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/",async(req,res)=>{
     try {
-        const response=await axios.get(apiUrl);
+        const response=await axios.get(apiUrl+"Any");
         const result=response.data;
         console.log(result.type)
         res.render("index.ejs",{joke:result});
@@ -20,9 +20,21 @@ app.get("/",async(req,res)=>{
         res.status(statusCode).send(error.message);
 
     }
+});
+
+app.post("/joke-by-category",async(req,res)=>{
+    const cat=req.body.category;
+    console.log(cat)
+    try {
+        const response=await axios.get(apiUrl+cat);
+        const result=response.data;
+        res.render("index.ejs",{joke:result});
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(404).send(error.message);
+    }
 })
-
-
 
 
 
